b5362690935991922d92b520835a0bb78de510bb
Leo Volwahsen <volwahsenl@gmail.com>
2023-01-28 12:30:57 +0100

    basic how to use typescript

---
+++ dest/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = [];
+names.push(1, 3, 77);
+console.log(names);
+console.log("Start of something new!");
+console.log("progress!");
+console.log("test: 1.01");
---
+++ src/script.ts
@@ -0,0 +1,6 @@
+let names: number[] = [];
+names.push(1, 3, 77);
+console.log(names);
+console.log("Start of something new!");
+console.log("progress!");
+console.log("test: 1.01");
