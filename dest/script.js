7295910a97fc836393a08ba7fc8eae605866cd39
Leo Volwahsen <volwahsenl@gmail.com>
2023-01-28 12:47:58 +0100

    trying arrays and touples

--- dest/script.js
+++ dest/script.js
@@ -5,3 +5,7 @@ console.log(names);
 console.log("Start of something new!");
 console.log("progress!");
 console.log("test: 1.01");
+let ourTouple;
+ourTouple = [20, false, "textIsWritten"];
+console.log(ourTouple);
+console.log(`the age was ${ourTouple[0]}, the text was ${ourTouple[1]}, but it read like this: ${ourTouple[2]}`);
