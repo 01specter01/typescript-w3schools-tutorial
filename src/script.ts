2aba2084ecfbea83cdf168a6acf8f8c7f697da80
Leo Volwahsen <volwahsenl@gmail.com>
2023-01-28 16:20:20 +0100

    change trext in html using ts

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,22 @@
-let names: number[] = [];
-names.push(1, 3, 77);
-console.log(names);
-console.log("Start of something new!");
-console.log("progress!");
-console.log("test: 1.01");
+// let names: number[] = [];
 
-let ourTouple: [number, boolean, string];
-ourTouple = [20, false, "textIsWritten"];
-console.log(ourTouple);
-console.log(
-    `the age was ${ourTouple[0]}, the text was ${ourTouple[1]}, but it read like this: ${ourTouple[2]}`
-);
+// names.push(1, 3, 77);
+// console.log(names);
+// console.log("Start of something new!");
+// console.log("progress!");
+// console.log("test: 1.01");
+
+// let ourTouple: [number, boolean, string];
+// ourTouple = [20, false, "textIsWritten"];
+// console.log(ourTouple);
+// console.log(
+//     `the age was ${ourTouple[0]}, the text was ${ourTouple[1]}, but it read like this: ${ourTouple[2]}`
+// );
+const app = document.getElementById("app");
+const p = document.createElement("p");
+app?.appendChild(p);
+const vehicles = ["mustang", "f1-50", "mercedes a-class"];
+const [mussleCar, truck, limosine] = vehicles;
+console.log(mussleCar);
+p.innerText = mussleCar;
+console.log("best");
